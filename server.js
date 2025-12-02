473cfb036f515a73cf744137070066544b80fc3d
Rushikesh Markad <markadrushikesh56@gmail.com>
2025-12-02 19:06:20 +0530

    Fix CORS for Render

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const userRoutes = require("./routes/userRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: "https://pt-frontend-9153.onrender.com",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+}));
+
+app.options("*", cors());  // <-- IMPORTANT
+
 app.use(bodyParser.json());
 
-// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/transactions", transactionRoutes);
 
@@ -17,7 +24,6 @@ app.get("/", (req, res) => {
     res.send("Personal Transaction Manager API Running");
 });
 
-// IMPORTANT: Render requires this
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
